be102a3f816041bdfaa54e3ae3a1e3c6d5139516
Julius Rückin <julius.rueckin@student.hpi.uni-potsdam.de>
2018-05-31 14:45:30 +0200

    Generate bibtex string from taxonomies if no bibtex variable is set

--- first-draft/static/static/js/script.js
+++ first-draft/static/static/js/script.js
@@ -5,7 +5,30 @@ $(document).ready(function(){
 	});
 
 	if(!parseInt($("#bibtex-available").val())){
-		alert("No bibtex available!");
+		var author = $("#author-field").val();
+		var title = $("#title-field").val();
+		var year = $("#year-field").val();
+
+		var bibtex_string = 
+						"@inproceedings{{0},<br>\
+						title={{1}},<br>\
+						author={{2}},<br>\
+						booktitle={Need to fill in},<br>\
+						year={{3}} }".format(generateBibtexKey(title, author, year),title, author, year);
+
+		$("#bibtex-dialog").html(bibtex_string);
 	}
 
 });
+
+function generateBibtexKey(title, author, year){
+	return (author.split(",")[0] + year + title.split(":")[0]).toLowerCase();
+}
+
+String.prototype.format = function() {
+	a = this;
+	for (k in arguments) {
+		a = a.replace("{" + k + "}", arguments[k]);
+	}
+	return a;
+}
