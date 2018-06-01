cc8c4cb5fac45a6f4bfa2a6b48ef811b9a610c62
Julius Rückin <julius.rueckin@student.hpi.uni-potsdam.de>
2018-06-01 09:52:36 +0200

    Change folder structure and build public files

---
+++ static/static/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+	$("#show-bibtex-btn").click(function(){
+		$("#bibtex-dialog").dialog();
+	});
+
+	if(!parseInt($("#bibtex-available").val())){
+		var author = $("#author-field").val();
+		var title = $("#title-field").val();
+		var year = $("#year-field").val();
+		var booktitle = $("#booktitle-field").val();
+
+		var bibtex_string = 
+						"@inproceedings{{0},<br>\
+						title={{1}},<br>\
+						author={{2}},<br>\
+						booktitle={{3}},<br>\
+						year={{4}} }".format(generateBibtexKey(title, author, year),title, author, booktitle, year);
+
+		$("#bibtex-dialog").html(bibtex_string);
+	}
+
+});
+
+function generateBibtexKey(title, author, year){
+	return (author.split(",")[0] + year + title.split(":")[0]).toLowerCase();
+}
+
+String.prototype.format = function() {
+	a = this;
+	for (k in arguments) {
+		a = a.replace("{" + k + "}", arguments[k]);
+	}
+	return a;
+}
